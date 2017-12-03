38a34dfb4c4b3b42944c3441ae176d17930ade72
fredrsf <kaznakhovskiy@gmail.com>
2017-12-03 22:13:09 +0300

    add so,e exs

---
+++ 1.4/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	//files := []string{"/Users/fredrsf/go/src/github.com/fredeastside/hello/1.txt"}
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		name := f.Name() + " -- " + input.Text()
+		counts[name]++
+	}
+}
---
+++ 1.7/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"net/http"
+	//"io/ioutil"
+	"io"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url 
+		}
+		response, err := http.Get(url)
+		if (err != nil) {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		//body, err := ioutil.ReadAll(response.Body)
+		body, err := io.Copy(os.Stdout, response.Body)
+		if (err != nil) {
+			fmt.Fprintf(os.Stderr, "fetch чтение %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%v\n", body)
+		fmt.Printf("Status code: %d\n", response.StatusCode)
+		fmt.Printf("Site: %s\n", url)
+	}
+}
